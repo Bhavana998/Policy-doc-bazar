f519c7ce6a9a2b110ca7df1e4873406e14f4618c
Bhavana <bhavanasetty95@gmail.com>
2026-03-27 18:19:22 +0530

    Fix import path for create_retrieval_chain

--- app/rag_pipeline.py
+++ app/rag_pipeline.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from app.retriever import get_faiss_index
